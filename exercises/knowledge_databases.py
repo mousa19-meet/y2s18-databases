2274ceb5b18cafd530aa423665202fbe8c214091
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 09:45:19 +0300

    too easy

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -8,8 +8,8 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+#works dont touch
 def add_article(article,topic,rating):
-
 	article_object = Knowledge(
 		article=article,
 		topic=topic,
@@ -17,20 +17,22 @@ def add_article(article,topic,rating):
 	session.add(article_object)
 	session.commit()
 
+#works dont touch
 def query_all_articles():
 	article = session.query(
 		Knowledge).all()
 	return article
 
-
-def query_article_by_rating(threshold):
-	rating = session.query(
+#works dont touch
+def query_article_by_rating(threshold,rating):
+	rating_object = session.query(
 		Knowledge).filter_by(
 		rating=rating).first()
-	if rating < threshold:
-		return rating
-	
-
+	if rating_object.rating < threshold:
+		session.query(Knowledge).filter_by(rating=rating).delete()
+		session.commit()
+		
+#works dont touch 
 def query_article_by_topic(topic):
 	topic = session.query(
 		Knowledge).filter_by(
@@ -38,27 +40,24 @@ def query_article_by_topic(topic):
 	return topic
 
 
+#works dont touch
 def delete_article_by_topic(topic):
 	session.query(Knowledge).filter_by(
 		topic=topic).delete()
 	session.commit()
 
 
+#works dont touch
 def delete_all_articles():
-
 	session.query(Knowledge).delete()
 	session.commit()
 
 
+#works dont touch
 def edit_article_rating(update_rating,article_title):
 	article_object = session.query(
 		Knowledge).filter_by(
 		article=article_title).first()
 	article_object.rating = update_rating
 	session.commit()
-
-
-
-
-
 
